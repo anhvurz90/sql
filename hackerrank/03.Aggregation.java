8e9730a1663b425923e4dffc9ce3ceec73059fb7
anhvurz90 <anhvurz90@gmail.com>
2017-08-05 19:58:29 +0700

    Add 03.Aggregation 7 -> 10

--- hackerrank/03.Aggregation.java
+++ hackerrank/03.Aggregation.java
@@ -10,4 +10,13 @@
   select sum(population) from city where countrycode='JPN';
 06.Population Density Difference:
   select (max(population) - min(population)) from city;
-
+07.The Blunder:
+  select ceil(avg(salary) - avg(replace(salary, '0', ''))) from employees;
+08.Top Earners:
+  select months * salary as e, count(*) from employee
+    group by e	  order by e desc	limit 1;
+09.Weather Observation station 2:
+  select round(sum(lat_n), 2), round(sum(long_w), 2) from station;
+10.Weather Observation station 13:
+  select truncate(sum(lat_n), 4) from station 
+	where lat_n > 38.788 and lat_n < 137.2345;
